f145adb32dd3a2f3e3ae639d22507c6fca72e4e6
Kittoru <mama190774@gmail.com>
2024-11-28 17:10:35 +0300

    feat: add answers for 3 tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -480,8 +480,8 @@ function unbracketTag(str) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
-  throw new Error('Not implemented');
+function extractEmails(str) {
+  return str.split(';');
 }
 
 /**
@@ -500,8 +500,20 @@ function extractEmails(/* str */) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+function encodeToRot13(str) {
+  const answ = [];
+  const newStr = str.split('');
+  const alp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const rot13 = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+  for (let i = 0; i < newStr.length; i += 1) {
+    const a = alp.indexOf(newStr[i]);
+    if (a !== -1) {
+      answ.push(rot13[a]);
+    } else {
+      answ.push(newStr[i]);
+    }
+  }
+  return answ.join('');
 }
 
 /**
@@ -528,8 +540,17 @@ function encodeToRot13(/* str */) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const cards =
+    'A♣ 2♣ 3♣ 4♣ 5♣ 6♣ 7♣ 8♣ 9♣ 10♣ J♣ Q♣ K♣ A♦ 2♦ 3♦ 4♦ 5♦ 6♦ 7♦ 8♦ 9♦ 10♦ J♦ Q♦ K♦ A♥ 2♥ 3♥ 4♥ 5♥ 6♥ 7♥ 8♥ 9♥ 10♥ J♥ Q♥ K♥ A♠ 2♠ 3♠ 4♠ 5♠ 6♠ 7♠ 8♠ 9♠ 10♠ J♠ Q♠ K♠';
+  const arr = cards.split(' ');
+  let answ = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] === value) {
+      answ = i;
+    }
+  }
+  return answ;
 }
 
 module.exports = {
